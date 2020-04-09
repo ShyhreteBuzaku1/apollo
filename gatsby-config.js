1cbb4b08bdfd6c81d24b21f1b5567c75f45e46da
ShyhreteBuzaku1 <52922504+ShyhreteBuzaku1@users.noreply.github.com>
2020-04-09 11:33:19 +0200

    logo changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Apollo',
+    description: 'Explore Moon, Forget your daily routine.',
+    author: '@Leta',
+    data: ['item1', 'item2'],
+  },
+
   plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-sass', `gatsby-plugin-styled-components`],
 };
